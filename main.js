d76658d18db81e85cf228c6b2b8216b9cea3800e
Sparticle999 <alexscruton@gmail.com>
2016-04-21 14:24:23 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,9 @@
+var money = 0;
 
+function buyShop {
+  var choice = prompt("Oh! It Does Not Look Like You Have Any Money! Here, I'll give you a loan! Is 1000 okay? Answer Yes/No");
+  if(choice === Yes || yes){
+    alert("And When Would You Like To Pay Off The Loan? ASAP or Never?")
+  }
+  
+}
