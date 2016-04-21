dc6a8c0292a45d64e0f760e85634a2c573d9b14e
Sparticle999 <alexscruton@gmail.com>
2016-04-21 14:36:58 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,10 +1,9 @@
 var money = 0;
-var loanChoice = 0; var loanChoice2 = 0;
 
 function buyShop() {
     var loanChoice = prompt("Oh! It Does Not Look Like You Have Any Money! Here, I'll give you a loan! Is 1000 okay, or would you like 50000?");
     if(loanChoice === "1000"){
-        var loadChoice2 = prompt("Great! And When Would You Like To Pay Off The Loan? ASAP or Never?");
+        var loanChoice2 = prompt("Great! And When Would You Like To Pay Off The Loan? ASAP or Never?");
         if(loanChoice2 === "ASAP"){
             alert("OK Then! Let's Get Started!");
         }
