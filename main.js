62590285dbbb9655df09a525358e6e50653d63a9
Sparticle999 <alexscruton@gmail.com>
2016-04-21 14:40:48 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,24 +1,20 @@
 var money = 0;
 
-function exit(){
-    
-}
-
 function buyShop() {
     var loanChoice = prompt("Oh! It Does Not Look Like You Have Any Money! Here, I'll give you a loan! Is 1000 okay, or would you like 50000?");
     if(loanChoice === "1000"){
         var loanChoice2 = prompt("Great! And When Would You Like To Pay Off The Loan? ASAP or Never?");
         if(loanChoice2 === "ASAP"){
             alert("OK Then! Let's Get Started!");
-            exit();
+            return;
         }
         if(loanChoice2 === "Never"){
             alert("ERROR! You Must Upgrade Your Decision Making Skills Before You Can Do This!");
-            exit();
+            return;
         }
         else{
             alert("That Was Not An Option! Please Try Again!");
-            exit();
+            return;
         }
     }
     if(loanChoice === "50000"){
