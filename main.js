2437e46998121589e565a76230e43ea590016fb9
Sparticle999 <alexscruton@gmail.com>
2016-04-21 14:30:33 +0100

    Update main.js

--- main.js
+++ main.js
@@ -2,10 +2,10 @@ var money = 0;
 
 function buyShop() {
     var loanChoice = prompt("Oh! It Does Not Look Like You Have Any Money! Here, I'll give you a loan! Is 1000 okay, or would you like 50000?");
-    if(loanChoice === 1000){
+    if(loanChoice === "1000"){
         var loadChoice2 = prompt("Great! And When Would You Like To Pay Off The Loan? ASAP or Never?");
     }
-    if(loanChoice === 50000){
+    if(loanChoice === "50000"){
         alert("ERROR! You Must Upgrade Your Decision Making Skills Before You Can Do This!");
     }
     else{
